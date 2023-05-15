4f971de47f27bcfdbd4c46769314ff7777fae583
Juan Giovanni <rodriguezjuanm@live.com>
2023-05-15 18:54:07 -0300

    feat vercel configuration 1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/',captchaRouter);
+app.use('/api',captchaRouter);
 app.get('/', (req, res)=>{
   res.send('Bienvenido ! te invito a "/generate-img"');
 })
